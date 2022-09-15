aa7bf87928c63f5e5de3ec279629a1030a243d94
Chipskein <abfn0905@gmail.com>
2022-09-15 14:40:52 -0300

    created base structure

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,10 @@
-console.log("testaando")
+import express from 'express';
+import usersRoutes from './users/routes.mjs';
+import albumsRoutes from './albums/routes.mjs';
+import musicsRoutes from './musics/routes.mjs';
+const PORT=process.env.PORT || 3001;  
+const app = express();
+app.use("/users",usersRoutes)
+app.use("/albums",albumsRoutes)
+app.use("/musics",musicsRoutes)
+app.listen(PORT,() => console.log(`Listing on PORT ${PORT}`))
