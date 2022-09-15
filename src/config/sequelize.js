eee385aae17583f626475cb5220d6ac4708b8630
Chipskein <abfn0905@gmail.com>
2022-09-15 15:53:37 -0300

    ue

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -8,22 +8,23 @@ import  Users  from '../entities/users/model.mjs'
 
 
 export function CreateSequelizeInstance(env="prod"){
-    if(env!="prod"){
-        return new Sequelize({
-            dialect: 'sqlite',
-            storage: '../database/test-database.db'
-        })    
+    if(env=="prod"){
+        const DATABASE_URL=process.env.DATABASE_URL;
+        const DATABASE_CONFIG={
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            },
+        }
+        return  new Sequelize(DATABASE_URL,DATABASE_CONFIG) 
     }
-    const DATABASE_URL=process.env.DATABASE_URL;
-    const DATABASE_CONFIG={
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-    }
-    return  new Sequelize(DATABASE_URL,DATABASE_CONFIG) 
+    return new Sequelize({
+        dialect: 'sqlite',
+        storage: '../database/test-database.db'
+    })    
+    
 }
 export function InitSequelizeModels(db){
     Albums.init(db)
