f20365bf43b00bd2d8523747990fba2bd32e61cf
Chipskein <abfn0905@gmail.com>
2022-09-15 21:23:50 -0300

    ue

--- src/entities/users/controller.mjs
+++ src/entities/users/controller.mjs
@@ -1,10 +1,44 @@
-export async function CreateUser(req,res){
+import { HTTP_STATUS } from '../../config/http-status.mjs'
+
+export function CreateUser(req,res){
+    try{
+        const { name,email,password } = req.body
+        
+        return res.status(HTTP_STATUS.OK).json({msg:"05_SUCCESS"})
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 export async function ListUser(req,res){
+    try{
+        
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 export async function GetUser(req,res){
+    try{
+        
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 export async function UpdateUser(req,res){
+    try{
+        
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 export async function DeleteUser(req,res){
+    try{
+        
+    }
+    catch(err){
+        console.log(err)
+    }
 }
--- src/entities/users/users.test.mjs
+++ src/entities/users/users.test.mjs
@@ -2,18 +2,26 @@ import {describe,expect,test} from 'vitest'
 import request  from 'supertest'
 
 import { CreateAppInstace } from '../../app.mjs'
-import { InitDatabaseTest,RemoveDatabaseTest } from '../../config/sequelize'
+//import { InitDatabaseTest,RemoveDatabaseTest } from '../../config/sequelize'
 
 const app=CreateAppInstace();
 
 describe("Testing Users Routes",async ()=>{
-    const db=await InitDatabaseTest()
-    test("POST users/",()=>{
-        console.log(db.models)
+    describe("POST users/",()=>{
+        
+        test("should Create User",async()=>{
+            const res=await request(app).post('/users/').send({
+                name:"eaiman",
+                email:"jafklajsfkljasklf",
+                password:"fçalklfç~kalsfk",
+            })
+            expect(res.statusCode).toBe(200)
+        })
+        
+        
     });
 
 
-    await RemoveDatabaseTest(db)
 })
 
 
