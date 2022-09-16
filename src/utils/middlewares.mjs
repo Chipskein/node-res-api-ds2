1db8a118d7c133bcd33091a88af07d9200b71d9e
Chipskein <abfn0905@gmail.com>
2022-09-15 22:22:48 -0300

    .

--- src/utils/middlewares.mjs
+++ src/utils/middlewares.mjs
@@ -1,3 +1,20 @@
-
+import { HTTP_STATUS } from '../config/http-status.mjs'
+import { verifyJWT } from './token.mjs';
 export async function verifyToken(req,res,next){
+    try{
+        const token = req.headers['authorization'];
+        if(!token){
+            return res.status(HTTP_STATUS.UNAUTHORIZED).json({
+                msg:"Empty Authorization Token"
+            })
+        }
+        const tokenInfo=verifyJWT(token)
+        req.data=tokenInfo;
+        next();
+    } catch(err){
+        return res.status(HTTP_STATUS.UNAUTHORIZED).json({
+            msg:"Invalid Authorization Token"
+        })
+    }
+    
 }
