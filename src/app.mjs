86f8b22ba31c66810645400c1be88f7e7113bec3
Chipskein <abfn0905@gmail.com>
2022-09-17 11:31:21 -0300

    adding more routes

--- src/app.mjs
+++ src/app.mjs
@@ -3,14 +3,24 @@ import usersRoutes from './entities/users/routes.mjs';
 import albumsRoutes from './entities/albums/routes.mjs';
 import musicsRoutes from './entities/musics/routes.mjs';
 import { InitDatabase } from './config/sequelize.mjs';
-
+import { HTTP_STATUS } from './config/http-status.mjs';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import path from 'path'
 export function CreateAppInstace(database){
     const app = express();
     InitDatabase(database)
     app.use(express.urlencoded({extended:true}));
     app.use(express.json());
+    app.get("/",(req,res)=>{
+        return res.sendFile(path.join(__dirname,'pages','index.html',))
+    })
     app.use("/users",usersRoutes)
     app.use("/albums",albumsRoutes)
     app.use("/musics",musicsRoutes)
+    app.use("*",(req,res)=>{
+        return res.status(HTTP_STATUS.NOT_FOUND).json({msg:"Resource not found"})
+    })
     return app;
 }
